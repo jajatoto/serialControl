f285ffc58f15f91ad32f700900fbb8ffcdbe5ac1
Niforances <sungyun1226@naver.com>
2019-11-28 20:04:30 +0900

    Update app.js
    temperature average calculating

--- iotNode_2rp/iotNode_2rp/app.js
+++ iotNode_2rp/iotNode_2rp/app.js
@@ -37,12 +37,15 @@ app.get('/:action', function (req, res) {
     if (action == "info") {
 
         arduinoSerialPort.write("i");
-        var buffer = ' ';
+        var buffer = '';
         arduinoSerialPort.on('data', function (dataChunk) {
 
             buffer += dataChunk;
+
             var data = buffer.split(/\r?\n/);
+
             console.log("현재 온도: 섭씨 " + parseFloat(data[0]) + "도 이며, 데이터베이스에 저장합니다.");
+
             client.query('INSERT INTO temp(temperature) VALUES(' + data[0] + ')', function (err, res) {
                 if (err) {
                     console.log("실패하였습니다.");
@@ -50,13 +53,39 @@ app.get('/:action', function (req, res) {
                 else {
                     console.log("성공적으로 쿼리를 보냈습니다.");
                 }
-            })
+
+            });
         });
+
         return res.send("데이터 수집 완료");
     }
+
     return res.send("Action: " + action);
 
 });
+
+app.get('/get/:action', function (req, res) {
+    var action = req.params.action || req.param('action');
+    if (action == 'temp') {
+
+        client.query('SELECT * FROM temp', function (res, err) {
+            var sum = 0;
+            var data = err.rows;
+            for (var i = 0; i < data.length; i++) {
+                sum += data[i]['temperature'];
+            }
+
+            var avg = sum / data.length;
+            console.log(avg);
+        });
+        return res.send("Complete");
+    }
+    if (action == 'bright') {
+        return res.send("in Progress");
+    }
+
+});
+
 app.listen(port, function () {
     console.log("http://0.0.0.0:" + port + " Server On.");
 });
