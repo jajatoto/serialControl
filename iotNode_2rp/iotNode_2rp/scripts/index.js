2ad4d24ebc4c1bc04f86112c1b7f0f46796ca608
Niforances <sungyun1226@naver.com>
2019-12-10 17:38:28 +0900

    [feat] Update index.js
    클릭이벤트 추가, 재사용을 위한 함수 분리

--- iotNode_2rp/iotNode_2rp/scripts/index.js
+++ iotNode_2rp/iotNode_2rp/scripts/index.js
@@ -1,20 +1,27 @@
 $(function () {
 
-    $("#btnQuery").click(function () {
-        $.get("http://127.0.0.1:3000/info", function (data) {
-            $("#pQuery").text(data);
-            $("#pQuery").fadeOut(100).fadeIn(200);
-        });
-    });
-
-    $("#btnInfo").click(function () {
-
+    function getTemp() {
         $.get("http://127.0.0.1:3000/get/temp", function (json) {
 
             $("#pTemp").text(JSON.stringify(json));
             $("#pTemp").fadeOut(100).fadeIn(200);
         })
+    }
 
+    function query() {
+        $.get("http://127.0.0.1:3000/info", function (data) {
+            $("#pQuery").text(data);
+            $("#pQuery").fadeOut(100).fadeIn(200);
+        });
+    }
+
+
+    $("#btnQuery").click(function () {
+        query();
+    });
+
+    $("#btnInfo").click(function () {
+        getTemp();
     });
 
     $("#btnAvg").click(function () {
@@ -24,4 +31,16 @@ $(function () {
         });
     });
 
+    $("#btnDel").click(function () {
+        if (confirm("정말로 삭제하시겠습니까?")) {
+
+            $.get("http://127.0.0.1:3000/del");
+            getTemp();
+            $("#pAvg").text("");
+
+        }
+        else {
+            alert("취소하였습니다.");
+        }
+    });
 });
