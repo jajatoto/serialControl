69e28fd6465ddfec1724f0c016a4efd92f53bc8e
Niforances <sungyun1226@naver.com>
2019-12-10 17:14:11 +0900

    [feat] Update index.js
    애니메이션 추가

--- iotNode_2rp/iotNode_2rp/scripts/index.js
+++ iotNode_2rp/iotNode_2rp/scripts/index.js
@@ -3,13 +3,16 @@ $(function () {
     $("#btnQuery").click(function () {
         $.get("http://127.0.0.1:3000/info", function (data) {
             $("#pQuery").text(data);
+            $("#pQuery").fadeOut(100).fadeIn(200);
         });
     });
 
     $("#btnInfo").click(function () {
 
         $.get("http://127.0.0.1:3000/get/temp", function (json) {
+
             $("#pTemp").text(JSON.stringify(json));
+            $("#pTemp").fadeOut(100).fadeIn(200);
         })
 
     });
@@ -17,6 +20,7 @@ $(function () {
     $("#btnAvg").click(function () {
         $.get("http://127.0.0.1:3000/get/avg", function (data) {
             $("#pAvg").text(data + "℃");
+            $("#pAvg").fadeOut(100).fadeIn(200);
         });
     });
 
